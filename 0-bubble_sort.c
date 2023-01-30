c8a0d02d75ece0c876e3aab2d560739c5453a081
ecigwegbu <igwegbu@gmail.com>
2023-01-30 08:50:07 +0100

    0. Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,30 +10,25 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int temp, swap = 1;
 
-	for (i = 0; i < (size - 1); i++)  /* i is starting cell */
-	/* i is starting cell; ending cell will be (size - i - 1) */
+	for (i = 0; i < (size - 1) && swap; i++)  /* i is starting cell */
+	/* i is starting cell; ending cell will be (size - 1 - i) */
 	{
-		printf("i: %lu\n", i);
-		if (1)
+		/* try swap */
+		swap = 0; /* reset swap indicator */
+		for (j = 0; j < size - 1 - i; j++)
+		/* j is swap cell */
 		{
-			/* try swap */
-			for (j = 0; j < size - 1 - i; j++)
-			/* j is swap cell */
+			/* now swap cells */
+			if (array[j + 1] < array[j])
 			{
-				/* now swap cells */
-				if (array[j + 1] < array[j])
-				{
-					temp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = temp;
-					/* swap = 0; */
-					print_array(array, size);
-				}
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swap = 1;
+				print_array(array, size);
 			}
-
-
 		}
 	}
 }
